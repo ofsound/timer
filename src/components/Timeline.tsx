061553ac728bef43d6ad4b9ed8a4c6de81ee0629
Ben Montgomery <ben@modernthings.net>
2025-07-13 16:16:08 -0600

    Streamline Timeline to just run

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,44 +1,32 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import Runner from "./Runner.tsx";
-
-import beepFile from "../assets/beep_01.wav";
 
 type inputProps = {
   currentArray: number[];
 };
 
 function Timeline({ currentArray }: inputProps) {
-  const beepAudio = useRef(new Audio(beepFile));
-
   const [runnerID, setRunnerID] = useState(0);
 
+  const handleIsRunning = (runnerRatio: number) => {
+    console.log(runnerRatio);
+  };
+
   const handleRunnerComplete = () => {
-    console.log("handleRunnerComplete called.");
-
-    console.log(runnerID);
-    console.log(currentArray.length);
-
-    beepAudio.current.play();
-
     if (runnerID < currentArray.length - 1) {
       setRunnerID(runnerID + 1);
     } else {
-      console.log("***");
+      console.log("Timeline Complete.");
     }
-  };
-
-  const handleIsRunning = (runnerRatio: number) => {
-    // setProgressRatio(runnerRatio);
-    console.log(runnerRatio);
   };
 
   return (
     <>
       <Runner
-        durationMilliseconds={1000 * currentArray[runnerID]}
-        runComplete={handleRunnerComplete}
         key={runnerID}
+        durationMilliseconds={1000 * currentArray[runnerID]}
         isRunning={handleIsRunning}
+        runComplete={handleRunnerComplete}
       />
     </>
   );
