dee771cd4e9e595c79a83b6c300ac36dafba6dbd
Ben Montgomery <ben@modernthings.net>
2025-07-28 15:58:32 -0600

    Rough in the conic gradient on the Start button

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -6,12 +6,15 @@ type inputProps = {
 
 function Start({ onClick, thisStep, thisRatio }: inputProps) {
   return (
-    <button
-      onClick={onClick}
-      className={`${thisStep === 0 && "animate-pulse"} block h-18 w-18 cursor-pointer rounded-full bg-green-800 px-3 py-3 text-white`}
-      style={{ transform: thisStep === 0 ? `rotate(${thisRatio * 360}deg)` : "rotate(0deg)" }}
-    >
-      Start
+    <button onClick={onClick} className="relative block h-18 w-18">
+      <div
+        className={`${thisStep === 0 && "animate-pulse"} via-green absolute top-0 h-18 w-18 cursor-pointer rounded-full bg-conic from-black to-green-500`}
+        style={{ transform: thisStep === 0 ? `rotate(${thisRatio * 360}deg)` : "rotate(0deg)" }}
+      ></div>
+      <div className="absolute top-0 mt-1 ml-1 h-16 w-16 rounded-full bg-black"></div>
+      <div className="absolute top-3 h-18 w-18 px-3 py-3 text-white">
+        {thisStep === 0 ? Math.ceil(5 - thisRatio * 5) : "START"}
+      </div>
     </button>
   );
 }
