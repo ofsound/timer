a52a35cc12db6fbbcb760003505ebcb52e7fd6e7
Ben Montgomery <ben@modernthings.net>
2025-08-30 13:27:52 -0600

    Rough in 2nd possible sound

--- src/components/Sound.tsx
+++ src/components/Sound.tsx
@@ -1,19 +1,33 @@
 import { forwardRef, useImperativeHandle, useEffect, useRef } from "react";
+
+import { useTimerStore } from "../store.ts";
+
 import beepFile from "../assets/beep_01.wav";
+import tremFile from "../assets/trem_01.mp3";
 
 const Sound = forwardRef((_props, ref) => {
+  const soundEffectIndex = useTimerStore((state) => state.soundEffectIndex);
+
   const beepAudio = useRef<HTMLAudioElement | null>(null);
+  const tremAudio = useRef<HTMLAudioElement | null>(null);
 
   useImperativeHandle(ref, () => ({
     play: () => {
-      if (beepAudio.current) {
-        beepAudio.current.play();
+      if (soundEffectIndex == 1) {
+        if (beepAudio.current) {
+          beepAudio.current.play();
+        }
+      } else {
+        if (tremAudio.current) {
+          tremAudio.current.play();
+        }
       }
     },
   }));
 
   useEffect(() => {
     beepAudio.current = new Audio(beepFile);
+    tremAudio.current = new Audio(tremFile);
   }, []);
 
   return <></>;
