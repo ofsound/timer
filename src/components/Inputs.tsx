ca40f9a1c89e5dc05d80d53f74795f17763590dd
Ben Montgomery <ben@modernthings.net>
2025-09-16 11:42:00 -0600

    Input Value editor

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useTimerStore } from "../store.ts";
 
 import { useSwipeable } from "react-swipeable";
+
+import { useLongPress } from "use-long-press";
 
 function Inputs() {
   const setThisSequence = useTimerStore((state) => state.setThisSequence);
@@ -9,7 +11,7 @@ function Inputs() {
   const setStartIsEnabled = useTimerStore((state) => state.setStartIsEnabled);
   const countInTime = useTimerStore((state) => state.countInTime);
 
-  const buttonValues = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 75, 80, 90];
+  const [buttonValues, setButtonValues] = useState([5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 75, 80, 90]);
 
   const padValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"];
 
@@ -18,6 +20,10 @@ function Inputs() {
   const [toggleVariant, setToggleVariant] = useState(false);
 
   const [padTotal, setPadTotal] = useState("");
+
+  const [customInputValue, setCustomInputValue] = useState(0);
+
+  const editedInputIndex = useRef(0);
 
   const buttonClickHandler = (buttonValue: number) => {
     if (!inputsAreEnabled) {
@@ -42,7 +48,7 @@ function Inputs() {
     setPadTotal("");
   };
 
-  const handlers = useSwipeable({
+  const handlersSwipe = useSwipeable({
     onSwiped: (eventData) => handleSwipe(eventData.dir),
     ...{
       delta: 10, // min distance(px) before a swipe starts.
@@ -55,24 +61,77 @@ function Inputs() {
     },
   });
 
+  const handlersPress = useLongPress((e) => {
+    const childElement = (e.target as HTMLElement).firstChild;
+    if (childElement) {
+      setCustomInputValue(parseInt((childElement as HTMLElement).innerHTML));
+    }
+
+    const parentElement = (e.target as HTMLElement).parentElement;
+    const children = parentElement?.children; // This is an HTMLCollection
+    const index = Array.prototype.indexOf.call(children, e.target as HTMLElement);
+    editedInputIndex.current = index;
+  });
+
   const handleSwipe = (direction: string) => {
     if (direction === "Left" || direction === "Right") {
       setToggleVariant(!toggleVariant);
     }
   };
 
+  const trySetCustomInputValue = (newValue: number) => {
+    setCustomInputValue(newValue);
+  };
+
+  const handleNewCustomValue = () => {
+    const tempButtonValues = [...buttonValues];
+    tempButtonValues[editedInputIndex.current] = customInputValue;
+
+    setButtonValues(tempButtonValues);
+  };
+
   return (
-    <div {...handlers}>
+    <div {...handlersSwipe}>
+      <div className="flex max-h-max self-center-safe">
+        <div className="relative flex">
+          <input
+            type="text"
+            className="pointer-events-none mt-4 mr-10 ml-auto h-14 w-14 rounded-md border-1 border-dotted bg-gray-100 pr-5 text-right text-2xl font-bold tabular-nums"
+            value={customInputValue}
+          />
+          <div className="flex flex-col gap-3">
+            <button
+              className="block h-10 w-10 rounded-sm border-1 border-dotted border-gray-900 bg-gray-100 text-2xl font-bold shadow-md"
+              onClick={() => trySetCustomInputValue(customInputValue + 1)}
+            >
+              <div className="relative -top-[2px]">+</div>
+            </button>
+            <button
+              className="block h-10 w-10 rounded-sm border-1 border-dotted border-gray-900 bg-gray-100 text-2xl font-bold shadow-md"
+              onClick={() => trySetCustomInputValue(customInputValue - 1)}
+            >
+              <div className="relative -top-[1px]">–</div>
+            </button>
+          </div>
+          <button onClick={handleNewCustomValue} className={`absolute -top-8 -right-10 block h-12 w-12 cursor-pointer`}>
+            <div className="mx-auto h-8 w-8 rounded-full border-1 border-white bg-gray-600">
+              <span className="relative -top-[4.5px] text-3xl text-white">×</span>
+            </div>
+          </button>
+        </div>
+      </div>
+
       <div
         className={` ${toggleVariant ? "hidden" : "flex"} mr-auto ml-auto max-h-1/4 w-full flex-wrap justify-between gap-[10px] pt-2`}
       >
         {buttonValues.map((item, index) => (
           <button
             onClick={() => buttonClickHandler(item)}
+            {...handlersPress()}
             key={index}
             className={`${!inputsAreEnabled ? "opacity-20 blur-[3px] grayscale" : "hover:border-blue-300"} flex aspect-1/1 max-h-3/7 w-1/6 grow-1 cursor-pointer items-center justify-center rounded-lg border border-blue-500 bg-blue-600 text-lg tracking-wider text-white`}
           >
-            <div>{item}</div>
+            <div className="pointer-events-none">{item}</div>
           </button>
         ))}
       </div>
